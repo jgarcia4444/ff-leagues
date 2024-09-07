eba97a42536a3fa48c1237345fcc81102c4cfc37
Jake Garcia <bballwiz4@gmail.com>
2024-09-06 23:28:42 -0600

    Created a get league info function and sent the info to the home header component.

---
+++ src/config/Urls.js
@@ -0,0 +1,10 @@
+
+// https://api.sleeper.app/v1/league/<league_id>/matchups/<week>
+const Urls = {
+    leagueUrl: "https://api.sleeper.app/v1/league/1129475036794073088",
+    matchupsUrl: "https://api.sleeper.app/v1/league/1129475036794073088/matchups/",
+    nflStateUrl: "https://api.sleeper.app/v1/state/nfl",
+    avatarUrl: "https://sleepercdn.com/avatars/",
+};
+
+export default Urls;
