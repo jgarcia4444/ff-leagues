9d433470fde06172a2f7bb129dd4563a7c525aa9
Jake Garcia <bballwiz4@gmail.com>
2024-09-08 21:51:48 -0600

    Addied in the useEffect and useState variables that will show the loading of the weeks matchups and hold the NFL week value as well as the matchups.

--- src/pages/Mathcups.js
+++ src/pages/Mathcups.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../shared/Layout';
 
 const Matchups = () => {
 
+    const [matchupInfo, setMatchupInfo] = useState();
+    const [leagueWeek, setLeagueWeek] = useState(0);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (leagueWeek === 0) {
+            // get nfl state to set the league week
+        } else {
+            // get the matchups for the week.
+        }
+    }, [leagueWeek])
+
     return (
         <Layout>
-            <h2 className="text-2xl">Matchups</h2>
+            
         </Layout>
     )
 }
