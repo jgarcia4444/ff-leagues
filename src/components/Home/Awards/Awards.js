aec0af4994efe0a2d0b9853457f22a03ed351ff4
Jake Garcia <bballwiz4@gmail.com>
2024-09-08 22:49:36 -0600

    set the league week in the useEffect and then fetch the week matchups.

--- src/components/Home/Awards/Awards.js
+++ src/components/Home/Awards/Awards.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import SectionTitle from '../../../shared/Titles/SectionTitle';
 
+import Urls from '../../../config/Urls';
+
 const Awards = () => {
+
+    const {nflStateUrl} = Urls;
 
     // REGULAR SEASON AWARDS
     // Most points for => The Rich Get Richer
--- src/pages/Mathcups.js
+++ src/pages/Mathcups.js
@@ -1,23 +1,38 @@
 import React, {useState, useEffect} from 'react';
+
 import Layout from '../shared/Layout';
+import Urls from '../config/Urls';
 
 const Matchups = () => {
 
-    const [matchupInfo, setMatchupInfo] = useState();
+    const {nflStateUrl, matchupsUrl} = Urls
+
+    const [matchupInfo, setMatchupInfo] = useState(null);
     const [leagueWeek, setLeagueWeek] = useState(0);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         if (leagueWeek === 0) {
-            // get nfl state to set the league week
-        } else {
+            fetch(nflStateUrl)
+                .then(res => res.json())
+                .then(data => {
+                    setLeagueWeek(data.display_week);
+                })
+        } else if (matchupInfo === null) {
             // get the matchups for the week.
+            const configuredUrl = `${matchupsUrl}${leagueWeek}`
+            fetch(configuredUrl)
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    setMatchupInfo(data);
+                })
         }
     }, [leagueWeek])
 
     return (
         <Layout>
-            
+
         </Layout>
     )
 }
