214e4c9c755120580ec9d99ff8175b4afd18252b
Jake Garcia <bballwiz4@gmail.com>
2024-09-22 23:00:32 -0600

    Added cases to the matchups reducer to deal with loading and saving the matchups data sent back from the api.

--- src/redux/reducers/MatchupsReducer/MatchupsReducer.js
+++ src/redux/reducers/MatchupsReducer/MatchupsReducer.js
@@ -6,6 +6,17 @@ const initialState = {
 
 const MatchupsReducer = (state=initialState, action) => {
     switch(action.type) {
+        case "FETCHING_MATCHUPS":
+            return {
+                ...state,
+                loading: true,
+            }
+        case "MATCHUPS_FETCHED":
+            return {
+                ...state,
+                loading: false,
+                matchups: action.matchups,
+            }
         default:
             return {
                 ...state,
