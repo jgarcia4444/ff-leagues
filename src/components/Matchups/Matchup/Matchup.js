bee420ac70e71ba2316fd863af08415210dce658
Jake Garcia <bballwiz4@gmail.com>
2024-09-29 00:10:05 -0600

    Matchup cards now show if a team is winning or losing.

--- src/components/Matchups/Matchup/Matchup.js
+++ src/components/Matchups/Matchup/Matchup.js
@@ -6,12 +6,10 @@ const Matchup = ({info}) => {
 
     const {team1, team2} = info;
 
-    const team1Winning = team1.points > team2.points ? true : false;
-
     return (
-        <div className="rounded bg-slate-100 flex flex-row item-center justify-evenly text-slate-600 bg-opacity-50 w-72 h-40 mb-6">
-            <MatchupTeam teamInfo={team1} />
-            <MatchupTeam teamInfo={team2} />
+        <div className="rounded bg-slate-100 flex flex-row item-center justify-evenly text-slate-600 bg-opacity-50 w-80 h-40 mb-6">
+            <MatchupTeam teamInfo={team1} winning={team1.points > team2.points} />
+            <MatchupTeam teamInfo={team2} winning={team2.points > team1.points} />
         </div>
     )
 }
--- src/components/Matchups/Matchup/MatchupTeam/MatchupTeam.js
+++ src/components/Matchups/Matchup/MatchupTeam/MatchupTeam.js
@@ -3,20 +3,19 @@ import React from 'react';
 
 import Urls from '../../../../config/Urls';
 
-const MatchupTeam = ({teamInfo}) => {
+const MatchupTeam = ({teamInfo, winning}) => {
 
     const {avatarUrl} = Urls;
-
 
     const {points, roster_id, settings, avatar_id, teamName, displayName} = teamInfo;
 
     const {wins, losses} = settings;
 
     return (
-        <div className="flex flex-col w-1/3 items-center justify-center">
+        <div className={`flex flex-col w-36 items-center justify-center ${winning === true ? 'bg-green-900' : 'bg-red-900'} bg-opacity-40`}>
             <img src={`${avatarUrl}${avatar_id}`} alt="" className="w-20 h-20 rounded-full" />
-            <div className="w-full">
-                <p className="text-sm text-left">{teamName === undefined ? displayName : teamName} </p>
+            <div className="w-full text-left ">
+                <p className="text-sm  font-bold text-left">{teamName === undefined ? displayName : teamName} </p>
             </div>
             <div className="w-full">
                 <p className="text-xs text-left">{wins}-{losses}</p>
