4c8c13a8cffff4ca516a12f83dad67b51490120e
Jake Garcia <bballwiz4@gmail.com>
2024-10-05 13:27:08 -0700

    connected the home page the redux store. added fetch rosters, getNflState, Rosters and NflState.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react'
+import { connect } from 'react-redux';
 
 import HomeHeader from '../components/Home/HomeHeader/HomeHeader';
 import Awards from '../components/Home/Awards/Awards';
 
 import Urls from '../config/Urls';
 import Layout from '../shared/Layout';
+import fetchRosters from '../redux/actions/rosters/fetchRosters';
+import getNflState from '../redux/actions/nflState/getNflState';
 
-const Home = () => {
+const Home = ({getNflState, fetchRosters, Rosters, NflState}) => {
 
     const {leagueUrl} = Urls;
 
@@ -37,4 +40,21 @@ const Home = () => {
     )
 }
 
-export default Home;
+const mapStateToProps = state =>  {
+    return {
+        Rosters: state.Rosters,
+        NflState: state.NflState
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchRosters: () => dispatch(fetchRosters()),
+        getNflState: () => dispatch(getNflState()),
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Home);
