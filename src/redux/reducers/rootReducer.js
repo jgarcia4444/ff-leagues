a1d24d000e931777e3d77ed69c9ec9b6c9f18486
Jake Garcia <bballwiz4@gmail.com>
2024-10-07 17:40:32 -0700

    Added the league reducer to the root reducer so it is accessible throughout the app

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -5,12 +5,14 @@ import MatchupsReducer from "./MatchupsReducer/MatchupsReducer";
 import NFLStateReducer from "./NFLStateReducer/NFLStateReducer";
 import RostersReducer from "./RostersReducer/RostersReducer";
 import UsersReducer from "./UsersReducer/UsersReducer";
+import LeagueReducer from "./LeagueReducer/LeagueReducer";
 
 const rootReducer = combineReducers({
     Matchups: MatchupsReducer,
     NflState: NFLStateReducer,
     Rosters: RostersReducer,
     Users: UsersReducer,
+    League: LeagueReducer
 });
 
 export default rootReducer;
