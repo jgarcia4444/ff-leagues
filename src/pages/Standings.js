12df3816d70b712479b0e901fcd372d908612463
Jake Garcia <bballwiz4@gmail.com>
2024-10-12 23:53:57 -0600

    Created the StandingCard component and added basic structure.

--- src/pages/Standings.js
+++ src/pages/Standings.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Layout from '../shared/Layout';
+import StandingCard from '../components/Standings/StandingCard/StandingCard';
 
-const Standings = () => {
+const Standings = ({rosters}) => {
+
+    const renderStandings = () => {
+        return rosters.map((roster, i) => <StandingCard key={`${i}-${roster.display_name}`} roster={roster} />)
+    }
 
     return (
         <Layout>
             <h2 className="text-2xl">Standings</h2>
+            <div className="">
+                {renderStandings()}
+            </div>
         </Layout>
     )
 };
 
-export default Standings;
+const mapStateToProps = state => {
+    return {
+        rosters: state.Rosters.rosters,
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    null
+)(Standings);
