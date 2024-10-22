ac52569ce22b00af9d493079f7ab48d313737cc1
Jake Garcia <bballwiz4@gmail.com>
2024-10-22 15:17:30 -0700

    Updated the BattleRoyale component

--- src/components/Matchups/BattleRoyale/BattleRoyale.js
+++ src/components/Matchups/BattleRoyale/BattleRoyale.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const BattleRoyale = () => {
+import BattleRoyaleRow from './BattleRoyaleRow/BattleRoyaleRow';
+
+const BattleRoyale = ({matchups}) => {
 
     const configureMatchups = () => {
-        
+        return matchups.sort((a, b) => {
+            return b.points - a.points
+        })
+    }
+
+    const renderMatchups = () => {
+        return configureMatchups().map((matchupInfo, i) => <BattleRoyaleRow key={`${i}-${matchupInfo.roster_id}`} info={matchupInfo} index={i}/>)
     }
 
     return (
-        <div className=""></div>
+        <div className="flex flex-col">
+            {renderMatchups()}
+        </div>
     )
 };
 
