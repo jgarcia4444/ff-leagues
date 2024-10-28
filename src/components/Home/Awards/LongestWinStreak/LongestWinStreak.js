d082d38723de80db9e35a4c36a1cb444ee6a67ad
Jake Garcia <bballwiz4@gmail.com>
2024-10-28 01:21:42 -0600

    Added the rest of the logic to calculate a rosters best win streak.

--- src/components/Home/Awards/LongestWinStreak/LongestWinStreak.js
+++ src/components/Home/Awards/LongestWinStreak/LongestWinStreak.js
@@ -9,23 +9,55 @@ const LongestWinStreak = ({users, rosters}) => {
         let recordArray = record.split("");
         let maxStreak = 0;
         let i = 0;
-        while (i < recordArray.length - 1) {
-            let currentNum = recordArray[i];
-            let nextNum = recordArray[i + 1];
-            if (currentNum === "W") {
-                if (currentNum === nextNum) {
-                    
+        if (recordArray.every(winOrLoss => winOrLoss === "W")) {
+            maxStreak = recordArray.length;
+        } else if (recordArray.every(winOrLoss => winOrLoss === "L")) {
+            maxStreak = 0;
+        } else {
+            let currentStreak = 0;
+            let i = 0;
+            while (i < recordArray.length - 1) {
+                let currentChar = recordArray[i];
+                if (currentChar === "W") {
+                    currentStreak = 1;
+                    let k = i + 1;
+                    let nextChar = recordArray[k];
+                    while (k < recordArray.length) {
+                        if (currentChar === nextChar) {
+                            currentStreak += 1;
+                        } else {
+                            break;
+                        }
+                        nextChar = recordArray[k + 1]
+                        k += 1;
+                    }
+                    if (currentStreak > maxStreak) {
+                        maxStreak = currentStreak;
+                    }
                 }
+                i += 1;
             }
         }
+        return maxStreak
     }
 
     const presentUser = () => {
         const rostersWithStreaks = rosters.map(roster => {
             const rosterStreak = 0;
             const {metadata} = roster;
-            rosterStreak = calculateWinningStreak(metadata.record)
+            rosterStreak = calculateWinningStreak(metadata.record);
+            return {
+                ...roster,
+                winStreak: rosterStreak,
+            }
         })
+        const rosterWithLongestStreak = rostersWithStreaks.filter((a, b) => b.winStreak - a.winStreak)[0];
+        const user = users.filter(user => user.user_id === rosterWithLongestStreak.owner_id)[0];
+        return (
+            <div className="">
+                
+            </div>
+        )
     }
 
     return (
