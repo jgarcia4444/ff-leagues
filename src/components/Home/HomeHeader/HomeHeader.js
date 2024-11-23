b11bcd2fc242479db1461de5129d386c80d5aa32
Jake Garcia <bballwiz4@gmail.com>
2024-11-22 21:38:22 -0700

    made it so the home header has the league title at the top of the image on smaller screens and to the right of the image on larger screens.

--- src/components/Home/HomeHeader/HomeHeader.js
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -10,8 +10,8 @@ const HomeHeader = ({leagueInfo}) => {
 
     return (
         <div className="w-full flex flex-col md:flex-row items-center mt-4">
-            <img src={`${avatarUrl}${avatar}`} alt="" className="w-full rounded " />
-            <div className="md:w-1/2 md:p-6 w-full mx-auto">
+            <img src={`${avatarUrl}${avatar}`} alt="" className="w-full rounded order-2 md:order-1" />
+            <div className="md:w-1/2 md:p-6 w-full mx-auto order-1 md:order-2 z-10">
                 <h3 className="text-6xl font-bold text-left text-slate-500">{name}</h3>
             </div>
         </div>
