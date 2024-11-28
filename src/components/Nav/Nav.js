1350b5bce5ef01c5e6aef40a15b0e58c664ade8e
Jake Garcia <bballwiz4@gmail.com>
2024-11-27 22:44:26 -0700

    Added in a hook to check the width of the screen.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { AiOutlineMenu } from "react-icons/ai";
+
 
 import NavLink from './NavLink/NavLink';
 import LogoNavLink from './LogoNavLink/LogoNavLink';
 
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+
 const Nav = () => {
+
+    const {width} = useWindowDimensions();
 
     const links = [
         {label: "Matchups", to: "/matchups"},
@@ -13,12 +19,34 @@ const Nav = () => {
         return links.map((link, i) => <NavLink linkInfo={link} key={`${link.label}-${i}`} />);
     }
 
-    return (
-        <section className="flex flex-row w-full justify-between items-center">
-            <LogoNavLink />
+    const mobileNav = () => {
+        return (
+            <div className="relative">
+
+            </div>
+        )
+    };
+
+    const nav = () => {
+        return (
             <div className="flex flex-row gap-10">
                 {renderNavLinks()}
             </div>
+        )
+    }
+
+    const renderNav = () => {
+        if (width < 688) {
+            return mobileNav();
+        } else {
+            return nav();
+        }
+    }
+
+    return (
+        <section className="flex flex-row w-full justify-between items-center">
+            <LogoNavLink />
+            {renderNav()}
         </section>
     )
 }
